e72785ceccc24af7eed9b485642b8ab8877e039f
Anil Manandhar <mdhanil03@gmail.com>
2025-10-07 21:45:45 +0545

    created reation timer using lifecycle hooks,creating timer,custom event with data

---
+++ ninja-reation-timer/src/App.vue
@@ -0,0 +1,62 @@
+<script>
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+export default {
+  name: 'App',
+  components: { Block ,Results},
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime){
+      this.score = reactionTime  
+      this.isPlaying = false 
+      this.showResults = true   
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Ninja reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button> 
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score" />
+
+</template>
+
+<style>
+#app{
+font-family: Avenir, Helvetica,Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align:center;
+color: #444;
+margin-top: 60px;
+}
+button{
+  background:#0faf87;
+  color:white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button [disabled] {
+opacity: 0.2;
+cursor: not-allowed;
+}
+</style>
