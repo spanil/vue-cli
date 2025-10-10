ba832d52b3f56682f044afecb22888f52fc406b9
anil23 <mdhanil03@gmail.com>
2025-10-10 22:53:05 +0545

    form/inouts v-model,select,checkboxes

--- form-inputs/src/App.vue
+++ form-inputs/src/App.vue
@@ -1,31 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import SignupForm from './components/SignupForm.vue';
+export default {
+  components:{SignupForm}
+
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<SignupForm />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 @media (min-width: 1024px) {
   header {
---
+++ form-inputs/src/components/SignupForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <form action="">
+        <label >Email</label>
+        <input type="email" required v-model="email">
+        <label >Password</label>
+        <input type="password" required v-model="password">     
+        <label>Role</label>
+        <select v-model="role">
+            <option value="developer">Web Developer</option>
+            <option value="designer">Web Designer</option>
+        </select>
+        <div class="terms">
+        <input type="checkbox" required v-model="terms">
+        <label for="">Accept</label>
+        </div>
+        <div>
+            <input type="checkbox" value="Hari" v-model="names">
+            <label for="">Hari</label>
+        </div>
+        <div>
+            <input type="checkbox" value="Shyam" v-model="names">
+            <label for="">shyam</label>
+        </div>
+        <div>
+            <input type="checkbox" value="Neta" v-model="names">
+            <label for="">Neta</label>
+        </div>
+    </form>
+    <p>Email : {{email}}</p>
+    <p>Password : {{password}}</p>
+    <p>Role : {{role}}</p>
+    <p>Accept Term : {{terms}}</p>
+    <p>Names : {{names}}</p>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            email:'test@sest.com',
+            password:'1234',
+            role: 'developer',
+            terms: false,
+            names :[]
+        }
+    }
+}
+</script>
+<style>
+</style>
