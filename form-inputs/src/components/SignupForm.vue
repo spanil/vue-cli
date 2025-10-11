c644751ee1f5c71e1f253c18d7ee89b33e2b2c97
Anil Manandhar <mdhanil03@gmail.com>
2025-10-11 21:53:24 +0545

    skills array,click event, submit event, validate password

--- form-inputs/src/components/SignupForm.vue
+++ form-inputs/src/components/SignupForm.vue
@@ -1,49 +1,131 @@
 <template>
-    <form action="">
+    <form @submit.prevent="handleSubmit">
         <label >Email</label>
         <input type="email" required v-model="email">
+
         <label >Password</label>
-        <input type="password" required v-model="password">     
+        <input type="password" required v-model="password"> 
+        <div v-if="passwordError" class="error">{{ passwordError }}</div>    
+
         <label>Role</label>
         <select v-model="role">
             <option value="developer">Web Developer</option>
             <option value="designer">Web Designer</option>
         </select>
+
+        <label for="">Skills</label>
+        <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+        <!-- splice method
+        <div v-for="(skill, index) in skills" class="pill" :key="skill">
+           <span @click="removeSkill(index)">{{ skill }}</span> 
+        </div> -->
+        <div v-for="skill in skills" class="pill" :key="skill">
+           <span @click="deleteSkill(skill)">{{ skill }}</span> 
+        </div>
+
         <div class="terms">
-        <input type="checkbox" required v-model="terms">
-        <label for="">Accept</label>
+            <input type="checkbox" required v-model="terms">
+            <label for="">Accept terms and Conditions</label>
         </div>
-        <div>
-            <input type="checkbox" value="Hari" v-model="names">
-            <label for="">Hari</label>
-        </div>
-        <div>
-            <input type="checkbox" value="Shyam" v-model="names">
-            <label for="">shyam</label>
-        </div>
-        <div>
-            <input type="checkbox" value="Neta" v-model="names">
-            <label for="">Neta</label>
-        </div>
+        <div class="submit">
+            <button>Create an Account</button>
+        </div>    
     </form>
-    <p>Email : {{email}}</p>
-    <p>Password : {{password}}</p>
-    <p>Role : {{role}}</p>
-    <p>Accept Term : {{terms}}</p>
-    <p>Names : {{names}}</p>
 </template>
 <script>
 export default {
     data(){
         return {
-            email:'test@sest.com',
-            password:'1234',
+            email:'',
+            password:'',
             role: 'developer',
             terms: false,
-            names :[]
+            tempSkill: '',
+            skills: [],
+            passwordError: ''
         }
+    },
+    methods:{
+        addSkill(e){
+            if(e.key === ',' && this.tempSkill){
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }                
+                this.tempSkill = ''
+            }
+    },
+    // splice method
+    // removeSkill(index){
+    //     this.skills.splice(index, 1)
+    // }
+    deleteSkill(skill){
+        this.skills = this.skills.filter((item)=>{
+            return skill != item
+        })
+    },
+    handleSubmit(){
+        this.passwordError = this.password.length > 5 ? '' : 'Password must be 6 chars long'
+        if(!this.passwordError){
+            console.log('email: ', this.email)
+            console.log('password: ',this.password)
+            console.log('role: ',this.role)
+            console.log('skills: ',this.skills)
+            console.log('terms accepted: ',this.terms)
     }
+    }
+    
+}
 }
 </script>
 <style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background:white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+
+label {
+    color:#aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input,select {
+    display: block;
+    padding: 10px 6px;
+    width:100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color:#555
+}
+.pill {
+display: inline-block;
+margin: 20px 10px 0 0;
+padding: 6px 12px;
+background: #eee;
+border-radius: 20px;
+font-size: 12px;
+letter-spacing: 1px;
+font-weight: bold;
+color:#777;
+cursor: pointer;
+}
+button {
+background:#eb6dff;
+border: 0;
+padding: 10px 28px;
+margin-top: 20px;
+color: white;
+border-radius: 20px
+}
+.submit{
+    text-align: center
+}
 </style>
