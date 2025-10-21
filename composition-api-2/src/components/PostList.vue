5691dc83429fb1aba6da5366fb52016ae27748e3
Anil Manandhar <mdhanil03@gmail.com>
2025-10-21 20:09:27 +0545

    composition api 2

---
+++ composition-api-2/src/components/PostList.vue
@@ -0,0 +1,12 @@
+<template>
+   <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+    </div>
+   </div> 
+</template>
+<script setup>
+const props = defineProps({
+  posts: Array
+})
+</script>
---
+++ composition-api-2/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <h1>Home</h1>
+  <PostList :posts="posts" />
+</template>
+  
+<script setup>
+import PostList from '@/components/PostList.vue';
+import { ref } from 'vue';
+const posts = ref([
+  {title: 'Welcome to the blog',body: 'Lorem ipsum',id: 1},
+  {title: 'top 5 css',body: 'Lorem ipsum',id: 2},
+])
+</script>
