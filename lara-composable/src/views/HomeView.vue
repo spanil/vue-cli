49662056682656cc5e9ea54ea03f2f7a156a81c4
Anil Manandhar <mdhanil03@gmail.com>
2025-11-12 15:09:39 +0545

    works on local storage

--- lara-composable/src/views/HomeView.vue
+++ lara-composable/src/views/HomeView.vue
@@ -1,11 +1,23 @@
 <script setup>
 import { useFlash } from '@/composables/useFlash';
+import { ref } from 'vue';
+let food = ref(localStorage.getItem('food'));
+let age = ref(localStorage.getItem('age'));
 
 let  {flash} = useFlash();
+function write(key, val){
+  localStorage.setItem(key,val)
+}
 </script>
 
 <template>
   <main>
+    <p>
+      what is your favourite food? <input type="text" v-model="food" @input="write('food',food)">
+    </p>
+    <p>
+      How Old are you ? <input type="text" v-model="age" @input="write('age',age)">
+    </p>
   <p> <button @click="flash('Test','It Works!')">Click Me</button></p>  
   </main>
 </template>
