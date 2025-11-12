89ea8dbd622d0f07705b8e66e70e67ca3cb37429
Anil Manandhar <mdhanil03@gmail.com>
2025-11-12 12:20:26 +0545

    use of composables by laracast

---
+++ lara-composable/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useFlash } from '@/composables/useFlash';
+
+let  {flash} = useFlash();
+</script>
+
+<template>
+  <div class="about">
+    <p> <button @click="flash('Yah','It Works on the about page','info')">Click Me</button></p>  
+  </div>
+</template>
+
+
---
+++ lara-composable/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useFlash } from '@/composables/useFlash';
+
+let  {flash} = useFlash();
+</script>
+
+<template>
+  <main>
+  <p> <button @click="flash('Test','It Works!')">Click Me</button></p>  
+  </main>
+</template>
